18091f98a7f7d15793473818f60a484800ce1c57
Dan <dan.weatherill@cantab.net>
2021-12-06 12:15:16 +0000

    add type annotation to models

--- inflammation/models.py
+++ inflammation/models.py
@@ -2,15 +2,15 @@
 
 The Model layer is responsible for the 'business logic' part of the software.
 
-Patients' data is held in an inflammation table (2D array) where each row contains 
-inflammation data for a single patient taken over a number of days 
+Patients' data is held in an inflammation table (2D array) where each row contains
+inflammation data for a single patient taken over a number of days
 and each column represents a single day across all patients.
 """
 
 import numpy as np
 
 
-def load_csv(filename):
+def load_csv(filename: str) -> np.ndarray:
     """Load a Numpy array from a CSV
 
     :param filename: Filename of CSV to load
@@ -18,9 +18,9 @@ def load_csv(filename):
     return np.loadtxt(fname=filename, delimiter=',')
 
 
-def daily_mean(data):
+def daily_mean(data: np.ndarray) -> np.ndarray:
     """Calculate the daily mean of a 2D inflammation data array.
-        
+
         Simply calles underlying numpy function
         :param data the array to find the daily mean of (2D numpy array, daily data row-wise)
         :returns array of mean values with daily means
@@ -28,21 +28,19 @@ def daily_mean(data):
     return np.mean(data, axis=0)
 
 
-def daily_max(data):
+def daily_max(data: np.ndarray) -> np.ndarray:
     """Calculate the daily max of a 2D inflammation data array.
-    
+
         Simply calles underlying numpy function
         :param data the data to find the daily mean of (2D numpy array, day data row-wise)
         :returns array of mean values with daily maxima
     """
-    
-    
     return np.max(data, axis=0)
 
 
-def daily_min(data):
+def daily_min(data: np.ndarray) -> np.ndarray:
     """Calculate the daily min of a 2D inflammation data array.
-    
+
         Simply calles underlying numpy function
         :param data the data to find the daily mean of (2D numpy array, day data row-wise)
         :returns array of mean values with daily minima
